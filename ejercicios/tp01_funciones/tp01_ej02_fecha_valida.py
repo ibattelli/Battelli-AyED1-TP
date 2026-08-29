1493878c95e9f3a76c5b5d88a5741b097f71b356
ibattelli <ibattelli17@gmail.com>
2026-08-29 01:07:17 -0300

    TP1_ej2

---
+++ ejercicios/tp01_funciones/tp01_ej02_fecha_valida.py
@@ -0,0 +1,62 @@
+def bisiesto(anio: int) -> bool:
+    """ Verifica si el año ingresado es bisiesto o no
+    Pre: anio debe ser un numero entero positivo
+    Post: si anio es bisiesto devuelte True, sino lo es, devuelve False
+    """
+    return (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
+
+def verificar_fecha(d: int, m: int, a:int ) -> bool:
+    """ Verifica si la fecha ingresa es correcta
+    Pre: d, m y a deben ser número enteros positivos
+    Post: devuelve True si la fecha es válida o False si la fecha no es válida
+    """
+    if d < 1 or d > 31:
+        return False
+    
+    if m < 1 or m > 12:
+        return False
+    
+    if a < 1:
+        return False
+
+    # verificación fecha correcta
+    if m in (1, 3, 5, 7, 8, 10, 12):
+        return True
+    elif m in (4, 6, 9, 11):
+        if d <= 30:
+            return True
+        else:
+            return False
+    
+    elif m == 2:
+        if bisiesto(a):
+            if d <= 29:
+                return True
+            else:
+                return False
+        else:
+            if d <= 28:
+                return True
+            else:
+                return False
+         
+
+def main():
+    dia = int(input("Ingrese el día: "))
+    mes = int(input("Ingrese el mes: "))
+    anio = int(input("Ingrese el año: "))
+    
+    if verificar_fecha(dia, mes, anio):
+        print("Es válido")
+    else:
+        print("Es inválido")
+
+
+assert verificar_fecha(5, 5, 2020) == True
+assert verificar_fecha(31, 12, 2021) == True
+assert verificar_fecha(30, 6, 2026) == True
+assert verificar_fecha(31, 6, 2020) == False
+assert verificar_fecha(29, 2, 2024) == True
+assert verificar_fecha(29, 2, 2023) == False
+
+main()
