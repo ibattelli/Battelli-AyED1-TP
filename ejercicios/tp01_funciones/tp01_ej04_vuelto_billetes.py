64e80cc8f8b1d8d71a4c4ac4c34ade1fbf5df873
ibattelli <ibattelli17@gmail.com>
2026-09-03 23:44:28 -0300

    TP1_ej4

---
+++ ejercicios/tp01_funciones/tp01_ej04_vuelto_billetes.py
@@ -0,0 +1,31 @@
+def cambio(total: int, recibido: int) -> None:
+    """ Calcula e informa la cantidad de billetes necesarios para dar como vuelto.
+
+    Pre: El total y recibido deben ser mayores a 0 y deben ser números enteros
+    Post: Informa la cantidad de billetes necesarios para dar como vuelto o si no es posible entregar el cambio.
+
+    """
+    assert total > 0, "El total deber ser mayor a 0"
+    assert recibido > 0, "Recibido debe ser mayor a 0"
+
+    if total > recibido:
+        print("Dinero insuficiente")
+    else:
+        vuelto = recibido - total
+        billetes = [5000, 1000, 500, 200, 100, 50, 10]
+        for b in billetes:
+            cantidad = vuelto // b
+            if cantidad > 0:
+                print(f"\nBilletes de ${b}: {cantidad}")
+                vuelto %= b
+
+        if vuelto > 0:
+            print("No se puede entregar el cambio.")
+    
+def main() -> None:
+    total = int(input("Ingrese el total de la compra: "))
+    recibido = int(input("Ingrese el dinero recibido: "))
+    cambio(total, recibido)
+
+if __name__ == "__main__":
+    main()
